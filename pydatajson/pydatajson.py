b53be17bd6cf2bc32ab05e22eee563b59deeb670
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2016-11-23 17:34:12 -0300

    Corrijo sugerencias de linter

--- pydatajson/pydatajson.py
+++ pydatajson/pydatajson.py
@@ -19,50 +19,46 @@ class DataJson(object):
     """Métodos para trabajar con archivos data.json."""
 
     # Variables por default
-    ABSOLUTE_PATH_TO_BASE_SCHEMA_DIR = os.path.join(os.getcwd(),
-                                                    "pydatajson/schemas")
-    DEFAULT_DATAJSON_SCHEMA_FILENAME = "required_fields_schema.json"
-    ABSOLUTE_PATH_TO_DEFAULT_SCHEMA_FILE = \
-            os.path.join(ABSOLUTE_PATH_TO_BASE_SCHEMA_DIR,
-                         DEFAULT_DATAJSON_SCHEMA_FILENAME)
+    ABSOLUTE_SCHEMA_DIR = os.path.join(os.getcwd(), "pydatajson/schemas")
+    DEFAULT_CATALOG_SCHEMA_FILENAME = "catalog.json"
 
 
-    def __init__(self, validator=None):
+    def __init__(self, schema_filename=None, schema_dir=None):
         """
         Args:
-            validator (object): Si se desea sobreescribir el validador por
-            default, se puede pasar uno a través de este parámetro.
+            schema_filename (str)   : Si se desea sobreescribir el validador por
         """
-        self.validator = (validator or
-                          self._create_validator(
-                              self.ABSOLUTE_PATH_TO_DEFAULT_SCHEMA_FILE))
+        schema_filename = (schema_filename or
+                           self.DEFAULT_CATALOG_SCHEMA_FILENAME)
+        schema_dir = schema_dir or self.ABSOLUTE_SCHEMA_DIR
 
+        self.validator = self._create_validator(schema_filename, schema_dir)
 
-    def _create_validator(self, schema_file):
-        with open(schema_file) as schema_file_buffer:
-            deserialized_schema = json.load(schema_file_buffer,
-                                            encoding="utf8")
+    @classmethod
+    def _create_validator(cls, schema_filename, schema_dir):
+
+        schema_path = os.path.join(schema_dir, schema_filename)
+        schema = cls._deserialize_json(schema_path)
+
         # Según https://github.com/Julian/jsonschema/issues/98
         # Permite resolver referencias locales a otros esquemas.
         resolver = jsonschema.RefResolver(
-            base_uri=("file://" + self.ABSOLUTE_PATH_TO_DEFAULT_SCHEMA_FILE),
-            referrer=deserialized_schema)
+            base_uri=("file://" + schema_dir + '/'), referrer=schema)
 
         validator = jsonschema.Draft4Validator(
-            schema=deserialized_schema,
-            resolver=resolver)
+            schema=schema, resolver=resolver)
 
         return validator
 
 
-    def _deserialize_datajson(self, datajson_file):
-        with open(datajson_file) as datajson_file_buffer:
-            datajson = json.load(datajson_file_buffer, encoding="utf8")
+    @staticmethod
+    def _deserialize_json(json_path):
+        """ Deserializa el archivo `json_path` y lo devuelve como
+        diccionario."""
+        with open(json_path) as json_file:
+            return json.load(json_file, encoding="utf8")
 
-        return datajson
-
-
-    def is_valid_catalog(self, datajson_file):
+    def is_valid_catalog(self, datajson_path):
         """Valida que el data.json cumple el datajson_schema.
 
         Chequea que el data.json tiene todos los campos obligatorios y que
@@ -76,15 +72,15 @@ class DataJson(object):
             bool: True si el data.json sigue el schema, sino False.
         """
         try:
-            self.validate_catalog(datajson_file)
-        except jsonschema.ValidationError as e:
-            print(e)
+            self.validate_catalog(datajson_path)
+        except jsonschema.ValidationError as error:
+            print(error)
             return False
         else:
             return True
 
 
-    def validate_catalog(self, datajson_file):
+    def validate_catalog(self, datajson_path):
         """Analiza el data.json registrando los errores que encuentra.
 
         Chequea que el data.json tiene todos los campos obligatorios y que
@@ -100,7 +96,7 @@ class DataJson(object):
         Returns:
             TODO: A definir.
         """
-        datajson = self._deserialize_datajson(datajson_file)
+        datajson = self._deserialize_json(datajson_path)
         self.validator.validate(datajson)
 
 
