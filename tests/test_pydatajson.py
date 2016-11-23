3be09030d687506a8db6084b5b6dc6a7fa548c81
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2016-11-23 12:11:43 -0300

    Creo objeto DataJson en setUp y agrego los tres tests de catalogos sin titulos

--- tests/test_pydatajson.py
+++ tests/test_pydatajson.py
@@ -14,25 +14,44 @@ import pydatajson
 class DataJsonTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.dj = pydatajson.DataJson()
 
     def tearDown(self):
-        pass
+        del(self.dj)
 
-    def test_is_valid_structure_full(self):
+    def test_is_valid_catalog_full(self):
         """Testea estructura de data.json completo bien formado."""
-        pass
 
-    def test_is_valid_structure_required_fields(self):
-        """Testea estructura de data.json (sólo campos obligatorios)."""
-
-        dj = pydatajson.DataJson("tests/samples/minimum_data.json")
-        res = dj.is_valid_structure()
+        res = self.dj.is_valid_catalog("tests/samples/full_data.json")
         self.assertTrue(res)
 
-        dj = pydatajson.DataJson("tests/samples/empty_minimum_data.json")
-        res = dj.is_valid_structure()
+    def test_is_valid_catalog_required_fields(self):
+        """Testea estructura de data.json que sólo contiene campos obligatorios)."""
+
+        res = self.dj.is_valid_catalog("tests/samples/minimum_data.json")
         self.assertTrue(res)
+
+        res = self.dj.is_valid_catalog("tests/samples/empty_minimum_data.json")
+        self.assertTrue(res)
+
+    def test_is_valid_catalog_missing_catalog_title(self):
+        """Testea estructura de data.json en el que un catálogo no tiene título."""
+
+        res = self.dj.is_valid_catalog("tests/samples/missing_catalog_title_data.json")
+        self.assertFalse(res)
+
+    def test_is_valid_catalog_missing_dataset_title(self):
+        """Testea estructura de data.json en el que un dataset no tiene título."""
+
+        res = self.dj.is_valid_catalog("tests/samples/missing_dataset_title_data.json")
+        self.assertFalse(res)
+
+    def test_is_valid_catalog_missing_resource_tile(self):
+        """Testea estructura de data.json en el que un recurso no tiene título."""
+
+        res = self.dj.is_valid_catalog("tests/samples/missing_resource_title_data.json")
+        self.assertFalse(res)
+
 
 
 if __name__ == '__main__':
