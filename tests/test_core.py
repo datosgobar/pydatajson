75ddc2e01e9a24d2a42d204998da31d564aa0c75
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2017-01-30 11:30:19 -0300

    Gran modificación a tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -16,8 +16,8 @@ import vcr
 from collections import OrderedDict
 import mock
 import filecmp
-import pydatajson
 import io
+from .context import pydatajson
 
 my_vcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
                  cassette_library_dir=os.path.join("tests", "cassetes"),
@@ -654,113 +654,6 @@ class DataJsonTestCase(unittest.TestCase):
 
         # `expected` es igual que en la prueba anterior.
         self.assertListEqual(actual, expected)
-
-    # TESTS DE _READ y _WRITE
-
-    CSV_TABLE = [
-        OrderedDict([(u'Plato', u'Milanesa'),
-                     (u'Precio', u'Bajo'),
-                     (u'Sabor', u'666')]),
-        OrderedDict([(u'Plato', u'Thoné, Vitel'),
-                     (u'Precio', u'Alto'),
-                     (u'Sabor', u'8000')]),
-        OrderedDict([(u'Plato', u'Aceitunas'),
-                     (u'Precio', u''),
-                     (u'Sabor', u'15')])
-    ]
-
-    XLSX_TABLE = [
-        OrderedDict([(u'Plato', u'Milanesa'),
-                     (u'Precio', u'Bajo'),
-                     (u'Sabor', 666)]),
-        OrderedDict([(u'Plato', u'Thoné, Vitel'),
-                     (u'Precio', u'Alto'),
-                     (u'Sabor', 8000)]),
-        OrderedDict([(u'Plato', u'Aceitunas'),
-                     (u'Precio', u'Gratis'),
-                     (u'Sabor', 15)])
-    ]
-
-    def test_read_table_from_csv(self):
-        csv_filename = os.path.join(self.SAMPLES_DIR, "read_table.csv")
-        actual_table = self.dj._read(csv_filename)
-        expected_table = self.CSV_TABLE
-
-        for (actual_row, expected_row) in zip(actual_table, expected_table):
-            self.assertEqual(actual_row, expected_row)
-
-    def test_read_table_from_xlsx(self):
-        xlsx_filename = os.path.join(self.SAMPLES_DIR, "read_table.xlsx")
-        actual_table = self.dj._read(xlsx_filename)
-        expected_table = self.XLSX_TABLE
-
-        for (actual_row, expected_row) in zip(actual_table, expected_table):
-            self.assertEqual(dict(actual_row), dict(expected_row))
-
-        self.assertListEqual(actual_table, self.XLSX_TABLE)
-
-    def test_write_table_to_csv(self):
-        expected_filename = os.path.join(self.RESULTS_DIR, "write_table.csv")
-        actual_filename = os.path.join(self.TEMP_DIR, "write_table.csv")
-
-        self.dj._write(self.CSV_TABLE, actual_filename)
-        comparison = filecmp.cmp(actual_filename, expected_filename)
-        if comparison:
-            os.remove(actual_filename)
-        else:
-            """
-{} se escribió correctamente, pero no es idéntico al esperado. Por favor,
-revíselo manualmente""".format(actual_filename)
-
-        self.assertTrue(comparison)
-
-    @unittest.skip("Requiere función auxiliar para comparar worksheets")
-    def test_write_table_to_xlsx(self):
-        expected_filename = os.path.join(self.RESULTS_DIR, "write_table.xlsx")
-        actual_filename = os.path.join(self.TEMP_DIR, "write_table.xlsx")
-
-        self.dj._write(self.XLSX_TABLE, actual_filename)
-        comparison = filecmp.cmp(actual_filename, expected_filename)
-        if comparison:
-            os.remove(actual_filename)
-        else:
-            """
-{} se escribió correctamente, pero no es idéntico al esperado. Por favor,
-revíselo manualmente""".format(actual_filename)
-
-        self.assertTrue(comparison)
-
-    def test_write_read_csv_loop(self):
-        """Escribir y leer un CSV es una operacion idempotente."""
-        temp_filename = os.path.join(self.TEMP_DIR, "write_read_loop.csv")
-        self.dj._write(self.CSV_TABLE, temp_filename)
-        read_table = self.dj._read(temp_filename)
-
-        comparison = (self.CSV_TABLE == read_table)
-        if comparison:
-            os.remove(temp_filename)
-        else:
-            """
-{} se escribió correctamente, pero no es idéntico al esperado. Por favor,
-revíselo manualmente""".format(temp_filename)
-
-        self.assertListEqual(read_table, self.CSV_TABLE)
-
-    @unittest.skip("No implementado aún")
-    def test_write_read_xlsx_loop(self):
-        """Escribir y leer un XLSX es una operacion idempotente."""
-        temp_filename = os.path.join(self.TEMP_DIR, "write_read_loop.xlsx")
-        self.dj._write(self.WRITEABLE_TABLE, temp_filename)
-        read_table = self.dj._read(temp_filename)
-
-        comparison = (self.XLSX_TABLE == read_table)
-        if comparison:
-            os.remove(temp_filename)
-            """
-{} se escribió correctamente, pero no es idéntico al esperado. Por favor,
-revíselo manualmente""".format(temp_filename)
-
-        self.assertListEqual(read_table, self.WRITEABLE_TABLE)
 
     def test_generate_datasets_summary(self):
         """Genera informe conciso sobre datasets correctamente."""
