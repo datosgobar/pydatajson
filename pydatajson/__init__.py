087913f83a98a058e220bccc7ead23e8f06cd898
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2017-01-30 11:38:13 -0300

    Corrijo __init__ de pydatjason y tests

--- pydatajson/__init__.py
+++ pydatajson/__init__.py
@@ -2,8 +2,7 @@
 # Módulo pydatajson
 # Conjunto de herramientas para validar y manipular la información presente en
 # el archivo `data.json` de un Portal de Datos
-from pydatajson import DataJson
-import xlsx_to_json
+from .core import DataJson
 
 __author__ = """Datos Argentina"""
 __email__ = 'datos@modernizacion.gob.ar'
