e7aae9873c0f432de53d3b0f66bb3f55d06c6fab
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2017-01-31 15:14:12 -0300

    Stubs de nuevos tests para metodos helpers

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -44,7 +44,7 @@ class HelpersTestCase(unittest.TestCase):
             expected_table = expected_tables[sheetname]
             self.assertEqual(actual_table, expected_table)
 
-    def test_string_to_list(self):
+    def test_string_to_list_default_separator(self):
         """string_to_list convierte una str separada por "," en una
         lista"""
         strings = [
@@ -63,12 +63,51 @@ class HelpersTestCase(unittest.TestCase):
             actual_list = pydatajson.helpers.string_to_list(string)
             self.assertListEqual(actual_list, expected_list)
 
-        # Pruebo con un separador especial
+    def test_string_to_list_alternative_separator(self):
+        """ string_to_list convierte una str separada por un separador
+        alternativo (";") en una lista."""
         actual_list = pydatajson.helpers.string_to_list(
             string="un;;separador;;nuevo", sep=";;")
         expected_list = ["un", "separador", "nuevo"]
 
         self.assertListEqual(actual_list, expected_list)
+
+    def test_traverse_dict_correct_keys(self):
+        """traverse_dict devuelve un valor si toda clave buscada existe."""
+        pass
+
+    def test_traverse_dict_index_out_of_range(self):
+        """traverse_dict devuelve el valor por omisión si un índice está fuera
+        del rango de su lista."""
+        pass
+
+    def test_traverse_dict_missing_key(self):
+        """traverse_dict devuelve el valor por omisión si una clave no existe
+        en un diccionario."""
+        pass
+
+    def test_traverse_dict_string_index_for_list(self):
+        """traverse_dict devuelve el valor por omisión si se pasa un string
+        como índice de una lista."""
+        pass
+
+    def test_is_list_of_matching_dicts_bad_input(self):
+        """is_list_of_matching_dicts levanta errores si el input no es una
+        lista compuesta únicamente de diccionarios."""
+        # input no lista
+        # lista con elemns no dict
+        pass
+
+    def test_is_list_of_matching_dicts_with_matched_dicts(self):
+        """is_list_of_matching_dicts devuelve True si todos los elementos del
+        input tienen las mismas claves."""
+        pass
+
+    def test_is_list_of_matching_dicts_with_mismatched_dicts(self):
+        """is_list_of_matching_dicts devuelve False si no todos los elementos
+        del input tienen las mismas claves."""
+        pass
+
 
 if __name__ == '__main__':
     nose.run(defaultTest=__name__)
