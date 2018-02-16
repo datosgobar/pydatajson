58e375e8c27c944e452b8293aedf7f744c1fe981
Lucas Romero <lromero@devartis.com>
2018-02-16 15:36:47 -0300

    Funcionalidad de remove_dataset para que filtre por un campo generico

--- pydatajson/federation.py
+++ pydatajson/federation.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 from ckanapi import RemoteCKAN
 from ckanapi.errors import NotFound
 from .ckan_utils import map_dataset_to_package
+from .search import get_datasets
 import re
 
 
@@ -58,22 +59,41 @@ def push_dataset_to_ckan(catalog, catalog_id, owner_org, dataset_origin_identifi
     return pushed_package['id']
 
 
-def remove_dataset_from_ckan(portal_url, apikey, identifier=None, organization=None, publisher=None):
+def remove_dataset_from_ckan(portal_url, apikey, filter_in=None, filter_out=None,
+                             only_time_series=False, organization=None):
+    """Borra un dataset en el portal pasado por parámetro.
+
+            Args:
+                portal_url (str): La URL del portal CKAN de destino.
+                apikey (str): La apikey de un usuario con los permisos que le permitan borrar el dataset.
+                filter_in(dict): Diccionario de filtrado positivo, similar al de search.get_datasets.
+                filter_out(dict): Diccionario de filtrado negativo, similar al de search.get_datasets.
+                only_time_series(bool): Filtrar solo los datasets que tengan recursos con series de tiempo.
+                organization(str): Filtrar solo los datasets que pertenezcan a cierta organizacion.
+            Returns:
+                None
+    """
     ckan_portal = RemoteCKAN(portal_url, apikey=apikey)
-    query = ''
-    if identifier:
-        query = 'id:"'+identifier+'"'
+    identifiers = []
+    datajson_filters = filter_in or filter_out or only_time_series
+    if datajson_filters:
+        identifiers += get_datasets(portal_url+'/data.json', filter_in=filter_in, filter_out=filter_out,
+                                    only_time_series=only_time_series, meta_field='identifier')
     if organization:
-        if query:
-            query += ' OR '
-        query += 'organization:"'+organization+'"'
-    if publisher:
-        if query:
-            query += ' OR '
-        query += 'author:"'+publisher+'"'
-    if query:
-        search_result = ckan_portal.call_action('package_search', data_dict={'q': query, 'rows': 500})
-        while search_result['count'] > 0:
-            for package in search_result['results']:
-                ckan_portal.call_action('dataset_purge', data_dict={'id': package['id']})
-            search_result = ckan_portal.call_action('package_search', data_dict={'q': query, 'rows': 500})
+        query = 'organization:"'+organization+'"'
+        search_result = ckan_portal.call_action('package_search', data_dict={'q': query, 'rows': 500, 'start': 0})
+        org_identifiers = [dataset['id'] for dataset in search_result['results']]
+        start = 500
+        while search_result['count'] > start:
+            search_result = ckan_portal.call_action('package_search',
+                                                    data_dict={'q': query, 'rows': 500, 'start': start})
+            org_identifiers += [dataset['id'] for dataset in search_result['results']]
+            start += 500
+
+        if datajson_filters:
+            identifiers = set(identifiers).intersection(set(org_identifiers))
+        else:
+            identifiers = org_identifiers
+
+    for identifier in identifiers:
+        ckan_portal.call_action('dataset_purge', data_dict={'id': identifier})
