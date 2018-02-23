d3afe59f1d447170005a7d3fe412b09971d84558
Lucas Romero <lromero@devartis.com>
2018-02-23 15:03:33 -0300

    Mantengo el keyword igual y de los themes subo el label en vez del id a tags

--- pydatajson/ckan_utils.py
+++ pydatajson/ckan_utils.py
@@ -6,12 +6,13 @@ from datetime import time
 from dateutil import parser, tz
 
 
-def append_attribute_to_extra(package, dataset, attribute, serialize=False):
+def append_attribute_to_extra(package, dataset, attribute, extra_attribute=None, serialize=False):
     value = dataset.get(attribute)
+    extra_attribute = extra_attribute or attribute
     if value:
         if serialize:
             value = json.dumps(value)
-        package['extras'].append({'key': attribute, 'value': value})
+        package['extras'].append({'key': extra_attribute, 'value': value})
 
 
 def map_dataset_to_package(dataset, catalog_id):
@@ -33,7 +34,7 @@ def map_dataset_to_package(dataset, catalog_id):
 
     super_themes = dataset['superTheme']
     package['groups'] = [{'name': re.sub(r'[^a-z-_]+', '', super_theme.lower())} for super_theme in super_themes]
-    package['extras'].append({'key': 'super_theme', 'value': json.dumps(super_themes)})
+    append_attribute_to_extra(package, dataset, 'superTheme', extra_attribute='super_theme', serialize=True)
 
 #   Recomendados y opcionales
     package['url'] = dataset.get('landingPage')
@@ -54,13 +55,9 @@ def map_dataset_to_package(dataset, catalog_id):
         package['maintainer'] = contact_point.get('fn')
         package['maintainer_email'] = contact_point.get('hasEmail')
 
-    package['tags'] = []
     keywords = dataset.get('keyword')
     if keywords:
-        package['tags'] += [{'name': re.sub(r'[^a-z-_]+', '', keyword.lower())} for keyword in keywords]
-    themes = dataset.get('theme')
-    if themes:
-        package['tags'] += [{'name': re.sub(r'[^a-z-_]+', '', theme.lower())} for theme in themes]
+        package['tags'] = [{'name': keyword} for keyword in keywords]
 
     return package
 
