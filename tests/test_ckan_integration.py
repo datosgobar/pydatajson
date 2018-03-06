a4ce6e9657bc57a89fef240b9034abb9593c8c7d
Agustín Benassi <agusbenassi@gmail.com>
2018-03-06 17:50:29 -0300

    Corrijo tests después de un cambio de nombre de la función para remover datasets de CKAN.

--- tests/test_ckan_integration.py
+++ tests/test_ckan_integration.py
@@ -5,14 +5,15 @@ import vcr
 from ckanapi import RemoteCKAN
 from ckanapi.errors import NotFound
 from .context import pydatajson
-from pydatajson.federation import push_dataset_to_ckan, remove_dataset_from_ckan
+from pydatajson.federation import push_dataset_to_ckan, remove_datasets_from_ckan
 
 SAMPLES_DIR = os.path.join("tests", "samples")
 
 
 class PushTestCase(unittest.TestCase):
     CKAN_VCR = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
-                       cassette_library_dir=os.path.join("tests", "cassetes", "ckan_integration", "push_dataset"),
+                       cassette_library_dir=os.path.join(
+                           "tests", "cassetes", "ckan_integration", "push_dataset"),
                        filter_headers=['Authorization', 'X-CKAN-API-Key'],
                        record_mode='once')
 
@@ -26,21 +27,26 @@ class PushTestCase(unittest.TestCase):
     @CKAN_VCR.use_cassette()
     def setUp(self):
         self.portal = RemoteCKAN(self.portal_url, apikey=self.apikey)
-        self.full_catalog = pydatajson.DataJson(self.get_sample('full_data.json'))
-        self.justice_catalog = pydatajson.DataJson(self.get_sample('catalogo_justicia.json'))
+        self.full_catalog = pydatajson.DataJson(
+            self.get_sample('full_data.json'))
+        self.justice_catalog = pydatajson.DataJson(
+            self.get_sample('catalogo_justicia.json'))
 
     @CKAN_VCR.use_cassette()
     def tearDown(self):
         full_dataset = self.full_catalog.datasets[0]
         full_name = re.sub(r'[^a-z-_]+', '', full_dataset['title'].lower())
         justice_dataset = self.justice_catalog.datasets[0]
-        justice_name = re.sub(r'[^a-z-_]+', '', justice_dataset['title'].lower())
+        justice_name = re.sub(
+            r'[^a-z-_]+', '', justice_dataset['title'].lower())
         try:
-            self.portal.call_action('dataset_purge', data_dict={'id': full_name})
+            self.portal.call_action(
+                'dataset_purge', data_dict={'id': full_name})
         except NotFound:
             pass
         try:
-            self.portal.call_action('dataset_purge', data_dict={'id': justice_name})
+            self.portal.call_action(
+                'dataset_purge', data_dict={'id': justice_name})
         except NotFound:
             pass
 
@@ -49,17 +55,19 @@ class PushTestCase(unittest.TestCase):
     @CKAN_VCR.use_cassette()
     def test_dataset_is_created_correctly(self):
         catalog = self.full_catalog
-        catalog_id = catalog.get('identifier', re.sub(r'[^a-z-_]+', '', catalog['title'].lower()))
+        catalog_id = catalog.get('identifier', re.sub(
+            r'[^a-z-_]+', '', catalog['title'].lower()))
         dataset = catalog.datasets[0]
         dataset_id = dataset['identifier']
         return_id = push_dataset_to_ckan(catalog, catalog_id, "oficina-de-muestra", dataset_id,
                                          self.portal_url, self.apikey)
-        self.assertEqual(return_id, catalog_id+'_'+dataset_id)
+        self.assertEqual(return_id, catalog_id + '_' + dataset_id)
 
     @CKAN_VCR.use_cassette()
     def test_dataset_is_updated_correctly(self):
         catalog = self.full_catalog
-        catalog_id = catalog.get('identifier', re.sub(r'[^a-z-_]+', '', catalog['title'].lower()))
+        catalog_id = catalog.get('identifier', re.sub(
+            r'[^a-z-_]+', '', catalog['title'].lower()))
         dataset_id = catalog.datasets[0]['identifier']
         push_dataset_to_ckan(catalog, catalog_id, "oficina-de-muestra", dataset_id,
                              self.portal_url, self.apikey)
@@ -68,22 +76,24 @@ class PushTestCase(unittest.TestCase):
         return_id = push_dataset_to_ckan(catalog, catalog_id, "oficina-de-muestra", dataset_id,
                                          self.portal_url, self.apikey)
 
-        data_dict = {'id': catalog_id+'_'+dataset_id}
+        data_dict = {'id': catalog_id + '_' + dataset_id}
         package = self.portal.call_action('package_show', data_dict=data_dict)
-        self.assertEqual(return_id, catalog_id+'_'+dataset_id)
+        self.assertEqual(return_id, catalog_id + '_' + dataset_id)
         self.assertEqual('updated description', package['notes'])
 
     @CKAN_VCR.use_cassette()
     def test_groups_are_created(self):
         catalog = self.full_catalog
-        catalog_id = catalog.get('identifier', re.sub(r'[^a-z-_]+', '', catalog['title'].lower()))
+        catalog_id = catalog.get('identifier', re.sub(
+            r'[^a-z-_]+', '', catalog['title'].lower()))
         dataset_id = catalog.datasets[0]['identifier']
         super_themes = catalog.datasets[0]['superTheme']
         super_themes = set(map(lambda x: x.lower(), super_themes))
 
         for s_theme in super_themes:
             try:
-                self.portal.call_action('group_delete', data_dict={'id': s_theme})
+                self.portal.call_action(
+                    'group_delete', data_dict={'id': s_theme})
             except NotFound:
                 continue
 
@@ -118,31 +128,42 @@ class PushTestCase(unittest.TestCase):
             justice_dataset['distribution'], full_dataset['distribution']
 
         data_dict = {'id': full_package_id}
-        full_package = self.portal.call_action('package_show', data_dict=data_dict)
+        full_package = self.portal.call_action(
+            'package_show', data_dict=data_dict)
         data_dict = {'id': justice_package_id}
-        justice_package = self.portal.call_action('package_show', data_dict=data_dict)
+        justice_package = self.portal.call_action(
+            'package_show', data_dict=data_dict)
 
-        self.assertEqual(len(full_package['resources']), len(justice_dataset['distribution']))
-        self.assertEqual(len(justice_package['resources']), len(full_dataset['distribution']))
+        self.assertEqual(len(full_package['resources']), len(
+            justice_dataset['distribution']))
+        self.assertEqual(len(justice_package['resources']), len(
+            full_dataset['distribution']))
 
         for resource, justice_distribution in zip(full_package['resources'], justice_dataset['distribution']):
-            self.assertEqual('same-catalog-id_'+justice_distribution['identifier'], resource['id'])
+            self.assertEqual('same-catalog-id_' +
+                             justice_distribution['identifier'], resource['id'])
 
         for resource, full_distribution in zip(justice_package['resources'], full_dataset['distribution']):
-            self.assertEqual('same-catalog-id_'+full_distribution['identifier'], resource['id'])
+            self.assertEqual('same-catalog-id_' +
+                             full_distribution['identifier'], resource['id'])
 
 
 class RemoveTestCase(unittest.TestCase):
     CKAN_VCR = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
-                       cassette_library_dir=os.path.join("tests", "cassetes", "ckan_integration", "remove_dataset"),
+                       cassette_library_dir=os.path.join(
+                           "tests", "cassetes", "ckan_integration", "remove_dataset"),
                        filter_headers=['Authorization', 'X-CKAN-API-Key'],
                        record_mode='once')
 
     test_datasets = [{'id': '1.1', 'owner_org': 'org-1', 'author': 'author_a', 'name': 'data1_1'},
-                     {'id': '2.1', 'owner_org': 'org-2', 'author': 'author_a', 'name': 'data2_1'},
-                     {'id': '2.2', 'owner_org': 'org-2', 'author': 'author_b', 'name': 'data2_2'},
-                     {'id': '3.1', 'owner_org': 'org-3', 'author': 'author_a', 'name': 'data3_1'},
-                     {'id': '3.2', 'owner_org': 'org-3', 'author': 'author_b', 'name': 'data3_2'},
+                     {'id': '2.1', 'owner_org': 'org-2',
+                         'author': 'author_a', 'name': 'data2_1'},
+                     {'id': '2.2', 'owner_org': 'org-2',
+                         'author': 'author_b', 'name': 'data2_2'},
+                     {'id': '3.1', 'owner_org': 'org-3',
+                         'author': 'author_a', 'name': 'data3_1'},
+                     {'id': '3.2', 'owner_org': 'org-3',
+                         'author': 'author_b', 'name': 'data3_2'},
                      {'id': '3.3', 'owner_org': 'org-3', 'author': 'author_c', 'name': 'data3_3'}]
 
     portal_url = 'http://localhost:8080'
@@ -153,7 +174,8 @@ class RemoveTestCase(unittest.TestCase):
         self.ckan_portal = RemoteCKAN(self.portal_url, apikey=self.apikey)
         for dataset in self.test_datasets:
             try:
-                self.ckan_portal.call_action('dataset_purge', data_dict={'id': dataset['id']})
+                self.ckan_portal.call_action(
+                    'dataset_purge', data_dict={'id': dataset['id']})
             except NotFound:
                 continue
         for dataset in self.test_datasets:
@@ -163,42 +185,50 @@ class RemoveTestCase(unittest.TestCase):
     def tearDown(self):
         for dataset in self.test_datasets:
             try:
-                self.ckan_portal.call_action('dataset_purge', data_dict={'id': dataset['id']})
+                self.ckan_portal.call_action(
+                    'dataset_purge', data_dict={'id': dataset['id']})
             except NotFound:
                 continue
-    
+
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_id(self):
         filter_in = {'dataset': {'identifier': '1.1'}}
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_in=filter_in)
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_in=filter_in)
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data1_1' not in package_list)
 
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_title(self):
         filter_in = {'dataset': {'title': 'data3_3'}}
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_in=filter_in)
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_in=filter_in)
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data3_3' not in package_list)
 
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_organization(self):
-        remove_dataset_from_ckan(self.portal_url, self.apikey, organization='org-2')
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, organization='org-2')
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data2_1' not in package_list)
         self.assertTrue('data2_2' not in package_list)
 
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_publisher_and_organization(self):
-        filter_in = {'dataset': {'publisher': {'name': 'author_b', 'mbox': None}}}
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_in=filter_in, organization='org-3')
+        filter_in = {'dataset': {'publisher': {
+            'name': 'author_b', 'mbox': None}}}
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_in=filter_in, organization='org-3')
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data3_2' not in package_list)
 
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_filter_out(self):
-        filter_out = {'dataset': {'publisher': {'name': 'author_b', 'mbox': None}}}
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_out=filter_out)
+        filter_out = {'dataset': {'publisher': {
+            'name': 'author_b', 'mbox': None}}}
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_out=filter_out)
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data2_2' in package_list)
         self.assertTrue('data3_2' in package_list)
@@ -207,8 +237,10 @@ class RemoveTestCase(unittest.TestCase):
 
     @CKAN_VCR.use_cassette()
     def test_remove_dataset_by_filter_out_and_organization(self):
-        filter_out = {'dataset': {'publisher': {'name': 'author_b', 'mbox': None}}}
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_out=filter_out, organization='org-3')
+        filter_out = {'dataset': {'publisher': {
+            'name': 'author_b', 'mbox': None}}}
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_out=filter_out, organization='org-3')
         package_list = self.ckan_portal.call_action('package_list')
         self.assertTrue('data3_1' not in package_list)
         self.assertTrue('data3_3' not in package_list)
@@ -218,7 +250,8 @@ class RemoveTestCase(unittest.TestCase):
         filter_in = {'dataset': {'identifier': '4.4'}}
         package_list_pre = self.ckan_portal.call_action('package_list')
 
-        remove_dataset_from_ckan(self.portal_url, self.apikey, filter_in=filter_in, organization='org-4')
+        remove_datasets_from_ckan(
+            self.portal_url, self.apikey, filter_in=filter_in, organization='org-4')
 
         package_list_post = self.ckan_portal.call_action('package_list')
         self.assertEqual(len(package_list_pre), len(package_list_post))
@@ -227,7 +260,7 @@ class RemoveTestCase(unittest.TestCase):
     def test_with_no_parametres(self):
         package_list_pre = self.ckan_portal.call_action('package_list')
 
-        remove_dataset_from_ckan(self.portal_url, self.apikey)
+        remove_datasets_from_ckan(self.portal_url, self.apikey)
 
         package_list_post = self.ckan_portal.call_action('package_list')
         self.assertEqual(len(package_list_pre), len(package_list_post))
--- tests/test_federation.py
+++ tests/test_federation.py
@@ -7,7 +7,7 @@ except ImportError:
     from unittest.mock import patch, MagicMock
 
 from .context import pydatajson
-from pydatajson.federation import push_dataset_to_ckan, remove_dataset_from_ckan
+from pydatajson.federation import push_dataset_to_ckan, remove_datasets_from_ckan
 from ckanapi.errors import NotFound
 
 SAMPLES_DIR = os.path.join("tests", "samples")
@@ -22,17 +22,20 @@ class PushDatasetTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.catalog = pydatajson.DataJson(cls.get_sample('full_data.json'))
-        cls.catalog_id = cls.catalog.get('identifier', re.sub(r'[^a-z-_]+', '', cls.catalog['title']).lower())
+        cls.catalog_id = cls.catalog.get('identifier', re.sub(
+            r'[^a-z-_]+', '', cls.catalog['title']).lower())
         cls.dataset = cls.catalog.datasets[0]
         cls.dataset_id = cls.dataset['identifier']
-        cls.minimum_catalog = pydatajson.DataJson(cls.get_sample('minimum_data.json'))
+        cls.minimum_catalog = pydatajson.DataJson(
+            cls.get_sample('minimum_data.json'))
         cls.minimum_catalog_id = cls.minimum_catalog.get('identifier',
                                                          re.sub(r'[^a-z-_]+', '', cls.minimum_catalog['title']).lower())
         cls.minimum_dataset = cls.minimum_catalog.datasets[0]
         # PATCH: minimum_data no tiene los identificadores obligatorios. Se los agrego aca, pero hay que refactorizar
         # tests y samples desactualizados para cumplir con los perfiles nuevos
         cls.minimum_dataset['identifier'] = cls.dataset['identifier']
-        cls.minimum_dataset['distribution'][0]['identifier'] = cls.dataset['distribution'][0]['identifier']
+        cls.minimum_dataset['distribution'][0][
+            'identifier'] = cls.dataset['distribution'][0]['identifier']
 
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_id_is_created_correctly(self, mock_portal):
@@ -46,7 +49,7 @@ class PushDatasetTestCase(unittest.TestCase):
         mock_portal.return_value.call_action = mock_call_action
         res_id = push_dataset_to_ckan(self.catalog, self.catalog_id, 'owner',
                                       self.dataset['identifier'], 'portal', 'key')
-        self.assertEqual(self.catalog_id+'_'+self.dataset_id, res_id)
+        self.assertEqual(self.catalog_id + '_' + self.dataset_id, res_id)
 
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_id_is_updated_correctly(self, mock_portal):
@@ -60,7 +63,7 @@ class PushDatasetTestCase(unittest.TestCase):
         mock_portal.return_value.call_action = mock_call_action
         res_id = push_dataset_to_ckan(self.catalog, self.catalog_id, 'owner',
                                       self.dataset['identifier'], 'portal', 'key')
-        self.assertEqual(self.catalog_id+'_'+self.dataset_id, res_id)
+        self.assertEqual(self.catalog_id + '_' + self.dataset_id, res_id)
 
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_new_groups_are_created(self, mock_portal):
@@ -129,7 +132,7 @@ class RemoveDatasetTestCase(unittest.TestCase):
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_empty_search_doesnt_call_purge(self, mock_portal):
         mock_portal.return_value.call_action = MagicMock()
-        remove_dataset_from_ckan('portal', 'key')
+        remove_datasets_from_ckan('portal', 'key')
         mock_portal.return_value.call_action.assert_not_called()
 
     @patch('pydatajson.federation.get_datasets')
@@ -138,8 +141,9 @@ class RemoveDatasetTestCase(unittest.TestCase):
         mock_portal.return_value.call_action = MagicMock()
         mock_search.return_value = ['some_id']
         filter_in = {'dataset': {'id': 'some_id'}}
-        remove_dataset_from_ckan('portal', 'key',  filter_in=filter_in)
-        mock_portal.return_value.call_action.assert_called_with('dataset_purge', data_dict={'id': 'some_id'})
+        remove_datasets_from_ckan('portal', 'key',  filter_in=filter_in)
+        mock_portal.return_value.call_action.assert_called_with(
+            'dataset_purge', data_dict={'id': 'some_id'})
 
     @patch('pydatajson.federation.get_datasets')
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
@@ -147,17 +151,22 @@ class RemoveDatasetTestCase(unittest.TestCase):
         mock_portal.return_value.call_action = MagicMock()
         mock_search.return_value = ['some_id', 'other_id']
         filter_out = {'dataset': {'id': 'some_id'}}
-        remove_dataset_from_ckan('portal', 'key', filter_out=filter_out)
-        mock_portal.return_value.call_action.assert_any_call('dataset_purge', data_dict={'id': 'other_id'})
-        mock_portal.return_value.call_action.assert_any_call('dataset_purge', data_dict={'id': 'some_id'})
+        remove_datasets_from_ckan('portal', 'key', filter_out=filter_out)
+        mock_portal.return_value.call_action.assert_any_call(
+            'dataset_purge', data_dict={'id': 'other_id'})
+        mock_portal.return_value.call_action.assert_any_call(
+            'dataset_purge', data_dict={'id': 'some_id'})
 
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_query_one_dataset(self, mock_portal):
-        mock_portal.return_value.call_action = MagicMock(return_value={'count': 1, 'results': [{'id': 'some_id'}]})
-        remove_dataset_from_ckan('portal', 'key',  organization='some_org')
+        mock_portal.return_value.call_action = MagicMock(
+            return_value={'count': 1, 'results': [{'id': 'some_id'}]})
+        remove_datasets_from_ckan('portal', 'key',  organization='some_org')
         data_dict = {'q': 'organization:"some_org"', 'rows': 500, 'start': 0}
-        mock_portal.return_value.call_action.assert_any_call('package_search', data_dict=data_dict)
-        mock_portal.return_value.call_action.assert_any_call('dataset_purge', data_dict={'id': 'some_id'})
+        mock_portal.return_value.call_action.assert_any_call(
+            'package_search', data_dict=data_dict)
+        mock_portal.return_value.call_action.assert_any_call(
+            'dataset_purge', data_dict={'id': 'some_id'})
 
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
     def test_query_over_500_datasets(self, mock_portal):
@@ -168,13 +177,17 @@ class RemoveDatasetTestCase(unittest.TestCase):
                         {'count': count, 'results': [{'id': 'id_3'}]},
                         None, None, None
                         ]
-        mock_portal.return_value.call_action = MagicMock(side_effect=side_effects)
-        remove_dataset_from_ckan('portal', 'key', organization='some_org')
+        mock_portal.return_value.call_action = MagicMock(
+            side_effect=side_effects)
+        remove_datasets_from_ckan('portal', 'key', organization='some_org')
         for start in range(0, count, 500):
-            data_dict = {'q': 'organization:"some_org"', 'rows': 500, 'start': start}
-            mock_portal.return_value.call_action.assert_any_call('package_search', data_dict=data_dict)
+            data_dict = {'q': 'organization:"some_org"',
+                         'rows': 500, 'start': start}
+            mock_portal.return_value.call_action.assert_any_call(
+                'package_search', data_dict=data_dict)
         for x in ['1', '2', '3']:
-            mock_portal.return_value.call_action.assert_any_call('dataset_purge', data_dict={'id': 'id_'+x})
+            mock_portal.return_value.call_action.assert_any_call(
+                'dataset_purge', data_dict={'id': 'id_' + x})
 
     @patch('pydatajson.federation.get_datasets')
     @patch('pydatajson.federation.RemoteCKAN', autospec=True)
@@ -182,6 +195,9 @@ class RemoveDatasetTestCase(unittest.TestCase):
         filter_results = ['id_1', 'id_2']
         org_results = [{'id': 'id_2'}, {'id': 'id_3'}]
         mock_search.return_value = filter_results
-        mock_portal.return_value.call_action = MagicMock(return_value={'count': 2, 'results': org_results})
-        remove_dataset_from_ckan('portal', 'key', only_time_series=True, organization='some_org')
-        mock_portal.return_value.call_action.assert_called_with('dataset_purge', data_dict={'id': 'id_2'})
+        mock_portal.return_value.call_action = MagicMock(
+            return_value={'count': 2, 'results': org_results})
+        remove_datasets_from_ckan(
+            'portal', 'key', only_time_series=True, organization='some_org')
+        mock_portal.return_value.call_action.assert_called_with(
+            'dataset_purge', data_dict={'id': 'id_2'})
