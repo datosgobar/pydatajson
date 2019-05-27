71a4aa1175d3c8faa91068ba90a6d78fa11ea56f
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-27 16:24:44 -0300

    Uso custom exception para nombre de formato

--- pydatajson/response_formatters/__init__.py
+++ pydatajson/response_formatters/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+from pydatajson import custom_exceptions
+
 from pydatajson.response_formatters.dict_formatter import DictFormatter
 from pydatajson.response_formatters.list_formatter import ListFormatter
 from pydatajson.response_formatters.tables_formatter import TablesFormatter
@@ -17,4 +19,4 @@ def format_response(validation, export_path, response_format):
         return formats[response_format].format()
     except KeyError:
         msg = "No se reconoce el formato {}".format(response_format)
-        raise Exception(msg)
+        raise custom_exceptions.FormatNameError(msg)
