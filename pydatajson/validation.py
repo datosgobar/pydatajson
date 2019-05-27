43ab28041f08d17d01cb7c4c3e8c4333f6d9edda
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-27 12:17:47 -0300

    Refactorizo código de validaciones en clase Validator

--- pydatajson/validation.py
+++ pydatajson/validation.py
@@ -11,11 +11,9 @@ from __future__ import with_statement, absolute_import
 
 import os
 import platform
-import requests
 import mimetypes
 import logging
 from collections import Counter
-from pprint import pprint
 
 try:
     from urlparse import urlparse
@@ -35,6 +33,183 @@ DEFAULT_CATALOG_SCHEMA_FILENAME = "catalog.json"
 EXTENSIONS_EXCEPTIONS = ["zip", "php", "asp", "aspx"]
 
 logger = logging.getLogger('pydatajson')
+
+
+class Validator(object):
+
+    def __init__(self, schema_filename=DEFAULT_CATALOG_SCHEMA_FILENAME,
+                 schema_dir=ABSOLUTE_SCHEMA_DIR):
+        self.jsonschema_validator = \
+            self.init_jsonschema_validator(schema_dir, schema_filename)
+
+    def init_jsonschema_validator(self, schema_dir, schema_filename):
+        schema_path = os.path.join(schema_dir, schema_filename)
+        schema = readers.read_json(schema_path)
+        # Según https://github.com/Julian/jsonschema/issues/98
+        # Permite resolver referencias locales a otros esquemas.
+        if platform.system() == 'Windows':
+            base_uri = "file:///" + schema_path.replace("\\", "/")
+        else:
+            base_uri = "file://" + schema_path
+        resolver = jsonschema.RefResolver(base_uri=base_uri, referrer=schema)
+        format_checker = jsonschema.FormatChecker()
+        return jsonschema.Draft4Validator(
+            schema=schema, resolver=resolver, format_checker=format_checker)
+
+    def is_valid(self, catalog):
+        return not self._get_errors(catalog)
+
+    def validate_catalog(self, catalog, only_errors=False):
+        default_response = self._default_response(catalog)
+        errors = self._get_errors(catalog)
+
+        response = default_response.copy()
+        for error in errors:
+            response = self._update_validation_response(
+                error, response)
+
+        # filtra los resultados que están ok, para hacerlo más compacto
+        if only_errors:
+            response["error"]["dataset"] = [
+                dataset for dataset in response["error"]["dataset"] if
+                dataset["status"] == "ERROR"]
+
+        return response
+
+    def _get_errors(self, catalog):
+        jsonschema_errors = list(
+            self.jsonschema_validator.iter_errors(catalog))
+        try:
+            custom_errors = self._custom_errors(catalog)
+        except:
+            logger.warning("Error de validación")
+            custom_errors = []
+        errors = jsonschema_errors + custom_errors
+        return errors
+
+    def _default_response(self, catalog):
+        return {
+            "status": "OK",
+            "error": {
+                "catalog": {
+                    "status": "OK",
+                    "title": catalog.get("title"),
+                    "errors": []
+                },
+                # "dataset" contiene lista de rtas default si el catálogo
+                # contiene la clave "dataset" y además su valor es una lista.
+                # En caso contrario "dataset" es None.
+                "dataset": [
+                    {
+                        "status": "OK",
+                        "title": dataset.get("title"),
+                        "identifier": dataset.get("identifier"),
+                        "list_index": index,
+                        "errors": []
+                    } for index, dataset in enumerate(catalog["dataset"])
+                ] if ("dataset" in catalog and
+                      isinstance(catalog["dataset"], list)) else None
+            }
+        }
+
+    def _custom_errors(self, catalog):
+        """Realiza validaciones sin usar el jsonschema.
+
+        En esta función se agregan bloques de código en python que realizan
+        validaciones complicadas o imposibles de especificar usando jsonschema
+        """
+        errors = []
+        for error in self._theme_ids_not_repeated(catalog):
+            errors.append(error)
+
+        for error in self._consistent_distribution_fields(catalog):
+            errors.append(error)
+
+        return errors
+
+    def _theme_ids_not_repeated(self, catalog):
+        if "themeTaxonomy" in catalog:
+            theme_ids = [theme["id"] for theme in catalog["themeTaxonomy"]]
+            dups = self._find_dups(theme_ids)
+            if len(dups) > 0:
+                yield ce.ThemeIdRepeated(dups)
+
+    def _consistent_distribution_fields(self, catalog):
+        for dataset_idx, dataset in enumerate(catalog["dataset"]):
+            for distribution_idx, distribution in enumerate(
+                    dataset["distribution"]):
+                for attribute in ['downloadURL', 'fileName']:
+                    if not self.format_matches_extension(distribution,
+                                                         attribute):
+                        yield ce.ExtensionError(dataset_idx, distribution_idx,
+                                                distribution, attribute)
+
+    def format_matches_extension(self, distribution, attribute):
+        """Chequea si una extensión podría corresponder a un formato dado."""
+
+        if attribute in distribution and "format" in distribution:
+            if "/" in distribution['format']:
+                possible_format_extensions = mimetypes.guess_all_extensions(
+                    distribution['format'])
+            else:
+                possible_format_extensions = [
+                    '.' + distribution['format'].lower()
+                ]
+
+            file_name = urlparse(distribution[attribute]).path
+            extension = os.path.splitext(file_name)[-1].lower()
+
+            if attribute == 'downloadURL' and not extension:
+                return True
+
+            # hay extensiones exceptuadas porque enmascaran otros formatos
+            if extension.lower().replace(".", "") in EXTENSIONS_EXCEPTIONS:
+                return True
+
+            if extension not in possible_format_extensions:
+                return False
+
+        return True
+
+    def _find_dups(self, elements):
+        return [item for item, count in Counter(elements).items()
+                if count > 1]
+
+    def _update_validation_response(self, error, response):
+        """Actualiza la respuesta por default acorde a un error de
+        validación."""
+        new_response = response.copy()
+
+        # El status del catálogo entero será ERROR
+        new_response["status"] = "ERROR"
+
+        # Adapto la información del ValidationError recibido a los fines
+        # del validador de DataJsons
+        error_info = {
+            # Error Code 1 para "campo obligatorio faltante"
+            # Error Code 2 para "error en tipo o formato de campo"
+            "error_code": 1 if error.validator == "required" else 2,
+            "message": error.message,
+            "validator": error.validator,
+            "validator_value": error.validator_value,
+            "path": list(error.path),
+            # La instancia validada es irrelevante si el error es de tipo 1
+            "instance": (None if error.validator == "required"
+                         else error.instance)
+        }
+
+        # Identifico a qué nivel de jerarquía sucedió el error.
+        if len(error.path) >= 2 and error.path[0] == "dataset":
+            # El error está a nivel de un dataset particular o inferior
+            position = new_response["error"]["dataset"][error.path[1]]
+        else:
+            # El error está a nivel de catálogo
+            position = new_response["error"]["catalog"]
+
+        position["status"] = "ERROR"
+        position["errors"].append(error_info)
+
+        return new_response
 
 
 def create_validator(schema_filename=None, schema_dir=None):
@@ -62,23 +237,7 @@ def create_validator(schema_filename=None, schema_dir=None):
     """
     schema_filename = schema_filename or DEFAULT_CATALOG_SCHEMA_FILENAME
     schema_dir = schema_dir or ABSOLUTE_SCHEMA_DIR
-    schema_path = os.path.join(schema_dir, schema_filename)
-    schema = readers.read_json(schema_path)
-
-    # Según https://github.com/Julian/jsonschema/issues/98
-    # Permite resolver referencias locales a otros esquemas.
-    if platform.system() == 'Windows':
-        base_uri = "file:///" + schema_path.replace("\\", "/")
-    else:
-        base_uri = "file://" + schema_path
-    resolver = jsonschema.RefResolver(base_uri=base_uri, referrer=schema)
-
-    format_checker = jsonschema.FormatChecker()
-
-    validator = jsonschema.Draft4Validator(
-        schema=schema, resolver=resolver, format_checker=format_checker)
-
-    return validator
+    return Validator(schema_filename, schema_dir)
 
 
 def is_valid_catalog(catalog, validator=None):
@@ -99,12 +258,9 @@ def is_valid_catalog(catalog, validator=None):
         if hasattr(catalog, "validator"):
             validator = catalog.validator
         else:
-            validator = create_validator()
+            validator = Validator()
 
-    jsonschema_res = validator.is_valid(catalog)
-    custom_errors = iter_custom_errors(catalog)
-
-    return jsonschema_res and len(list(custom_errors)) == 0
+    return validator.is_valid(catalog)
 
 
 def validate_catalog(catalog, only_errors=False, fmt="dict",
@@ -159,52 +315,14 @@ def validate_catalog(catalog, only_errors=False, fmt="dict",
     """
     catalog = readers.read_catalog(catalog)
 
-    # La respuesta por default se devuelve si no hay errores
-    default_response = {
-        "status": "OK",
-        "error": {
-            "catalog": {
-                "status": "OK",
-                "title": catalog.get("title"),
-                "errors": []
-            },
-            # "dataset" contiene lista de rtas default si el catálogo
-            # contiene la clave "dataset" y además su valor es una lista.
-            # En caso contrario "dataset" es None.
-            "dataset": [
-                {
-                    "status": "OK",
-                    "title": dataset.get("title"),
-                    "identifier": dataset.get("identifier"),
-                    "list_index": index,
-                    "errors": []
-                } for index, dataset in enumerate(catalog["dataset"])
-            ] if ("dataset" in catalog and
-                  isinstance(catalog["dataset"], list)) else None
-        }
-    }
-
     # Genero la lista de errores en la instancia a validar
     if not validator:
         if hasattr(catalog, "validator"):
             validator = catalog.validator
         else:
             validator = create_validator()
-    jsonschema_errors = list(validator.iter_errors(catalog))
-    custom_errors = list(iter_custom_errors(catalog))
 
-    errors = jsonschema_errors + custom_errors
-
-    response = default_response.copy()
-    for error in errors:
-        response = _update_validation_response(
-            error, response)
-
-    # filtra los resultados que están ok, para hacerlo más compacto
-    if only_errors:
-        response["error"]["dataset"] = [
-            dataset for dataset in response["error"]["dataset"] if
-            dataset["status"] == "ERROR"]
+    response = validator.validate_catalog(catalog, only_errors)
 
     # elige el formato del resultado
     if export_path:
@@ -267,102 +385,6 @@ def validate_catalog(catalog, only_errors=False, fmt="dict",
         raise Exception("No se reconoce el formato {}".format(fmt))
 
 
-def iter_custom_errors(catalog):
-    """Realiza validaciones sin usar el jsonschema.
-
-    En esta función se agregan bloques de código en python que realizan
-    validaciones complicadas o imposibles de especificar usando jsonschema.
-    """
-
-    try:
-        # chequea que no se repiten los ids de la taxonomía específica
-        if "themeTaxonomy" in catalog:
-            theme_ids = [theme["id"] for theme in catalog["themeTaxonomy"]]
-            dups = _find_dups(theme_ids)
-            if len(dups) > 0:
-                yield ce.ThemeIdRepeated(dups)
-
-        # chequea que un dataset no use un theme que no exista en la taxonomía
-        # TODO: hay que implementarlo
-
-        # chequea que un dataset no use theme con id esté repetido en taxonomía
-        # TODO: hay que implementarlo
-
-        # chequea que la extensión de fileName, downloadURL y format sean
-        # consistentes
-        for dataset_idx, dataset in enumerate(catalog["dataset"]):
-            for distribution_idx, distribution in enumerate(
-                    dataset["distribution"]):
-                for attribute in ['downloadURL', 'fileName']:
-                    if not format_matches_extension(distribution, attribute):
-                        yield ce.ExtensionError(dataset_idx, distribution_idx,
-                                                distribution, attribute)
-
-        # chequea que no haya duplicados en los downloadURL de las
-        # distribuciones
-        # urls = []
-        # for dataset in catalog["dataset"]:
-        #     urls += [distribution['downloadURL']
-        #              for distribution in dataset['distribution']
-        #              if distribution.get('downloadURL')]
-        # dups = _find_dups(urls)
-        # if len(dups) > 0:
-        #     yield ce.DownloadURLRepetitionError(dups)
-
-        # chequea que las URLs de las distribuciones estén operativas
-        # hay que revisar
-        # for distribution in catalog.distributions:
-        #     url = distribution.get("downloadURL")
-        #     status_code = requests.head(url).status_code
-        #     if status_code != 200:
-        #         yield ce.DownloadURLBrokenError(
-        #             distribution.get("identifier"), url, status_code)
-
-    except Exception:
-        logger.exception("Error de validación.")
-
-
-def _find_dups(elements):
-    return [item for item, count in Counter(elements).items()
-            if count > 1]
-
-
-def _update_validation_response(error, response):
-    """Actualiza la respuesta por default acorde a un error de
-    validación."""
-    new_response = response.copy()
-
-    # El status del catálogo entero será ERROR
-    new_response["status"] = "ERROR"
-
-    # Adapto la información del ValidationError recibido a los fines
-    # del validador de DataJsons
-    error_info = {
-        # Error Code 1 para "campo obligatorio faltante"
-        # Error Code 2 para "error en tipo o formato de campo"
-        "error_code": 1 if error.validator == "required" else 2,
-        "message": error.message,
-        "validator": error.validator,
-        "validator_value": error.validator_value,
-        "path": list(error.path),
-        # La instancia validada es irrelevante si el error es de tipo 1
-        "instance": (None if error.validator == "required" else error.instance)
-    }
-
-    # Identifico a qué nivel de jerarquía sucedió el error.
-    if len(error.path) >= 2 and error.path[0] == "dataset":
-        # El error está a nivel de un dataset particular o inferior
-        position = new_response["error"]["dataset"][error.path[1]]
-    else:
-        # El error está a nivel de catálogo
-        position = new_response["error"]["catalog"]
-
-    position["status"] = "ERROR"
-    position["errors"].append(error_info)
-
-    return new_response
-
-
 def _catalog_validation_to_list(response):
     """Formatea la validación de un catálogo a dos listas de errores.
 
@@ -417,51 +439,3 @@ def _catalog_validation_to_list(response):
             rows_dataset.append(dataset_result)
 
     return {"catalog": rows_catalog, "dataset": rows_dataset}
-
-
-def format_matches_extension(distribution, attribute):
-    """Chequea si una extensión podría corresponder a un formato dado."""
-
-    if attribute in distribution and "format" in distribution:
-        if "/" in distribution['format']:
-            possible_format_extensions = mimetypes.guess_all_extensions(
-                distribution['format'])
-        else:
-            possible_format_extensions = ['.' + distribution['format'].lower()]
-
-        file_name = urlparse(distribution[attribute]).path
-        extension = os.path.splitext(file_name)[-1].lower()
-
-        if attribute == 'downloadURL' and not extension:
-            return True
-
-        # hay extensiones exceptuadas porque enmascaran otros formatos
-        if extension.lower().replace(".", "") in EXTENSIONS_EXCEPTIONS:
-            return True
-
-        if extension not in possible_format_extensions:
-            return False
-
-    return True
-
-
-def main(catalog):
-    """Permite validar un catálogo por línea de comandos.
-
-    Args:
-        catalog (str): Path local o URL a un catálogo.
-    """
-    res = validate_catalog(catalog, only_errors=True, fmt="list",
-                           export_path=None, validator=None)
-    print("")
-    print("=== Errores a nivel de CATALOGO ===")
-    pprint(res["catalog"])
-
-    print("")
-    print("=== Errores a nivel de DATASET ===")
-    pprint(res["dataset"])
-
-
-if __name__ == '__main__':
-    args = sys.argv[1:] if len(sys.argv > 1) else []
-    main(*args)
