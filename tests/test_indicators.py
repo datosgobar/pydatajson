83ec01c6aba4a19abcf3e0ff1c4117278c38d648
fcassanello <fcassanello@gitlab.com>
2019-09-13 13:17:38 -0300

    Agrego tests

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -9,6 +9,8 @@ import os.path
 import vcr
 from nose.tools import assert_true, assert_false, assert_equal
 
+from pydatajson.indicators import _eventual_periodicity
+
 try:
     import mock
 except ImportError:
@@ -624,3 +626,8 @@ class TestIndicatorsTestCase(object):
 
         for k, v in expected.items():
             assert_equal(indicators[k], v)
+
+    def test_eventual_periodicity(self):
+        assert_true(_eventual_periodicity('eventual'))
+        assert_true(_eventual_periodicity('EVENTUAL'))
+        assert_false(_eventual_periodicity('not eventual'))
