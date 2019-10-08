f5c863fd6c021a2c3a59333534f64474cbfa9644
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-10-08 10:23:19 -0300

    cierro pool de threads

--- pydatajson/threading_helper.py
+++ pydatajson/threading_helper.py
@@ -3,5 +3,7 @@ from multiprocessing.pool import ThreadPool
 
 def apply_threading(l, function, cant_threads):
     pool = ThreadPool(processes=cant_threads)
-
-    return pool.map(function, l)
+    results = pool.map(function, l)
+    pool.close()
+    pool.join()
+    return results
