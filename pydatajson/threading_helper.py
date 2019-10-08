ca8d0644e5f564797d3c8ea0796aa5426e82f98e
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-10-08 12:00:19 -0300

    Optimización en caso un thread

--- pydatajson/threading_helper.py
+++ pydatajson/threading_helper.py
@@ -2,6 +2,8 @@ from multiprocessing.pool import ThreadPool
 
 
 def apply_threading(l, function, cant_threads):
+    if cant_threads == 1:
+        return [function(x) for x in l]
     pool = ThreadPool(processes=cant_threads)
     results = pool.map(function, l)
     pool.close()
